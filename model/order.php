9651aa17d63fad8985189794fef43d07545422dd
saurabh <saurabh@schoolcom.in>
2015-09-19 15:12:27 +0530

    edit model

---
+++ model/order.php
@@ -0,0 +1,37 @@
+<?php
+
+class Order
+{  
+
+   
+   function __construct($email_id){
+   }
+
+   function create(){
+
+   	
+   }
+
+   function update(){
+
+
+   }
+
+   function today_order(){
+
+   	$date = date("Y-m-d:h:i:s");
+
+   }
+
+   function delete(){
+
+   }
+
+   function index(){
+
+
+   }
+
+ }
+
+?>
